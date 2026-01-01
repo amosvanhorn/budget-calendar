12d7496f183baa70113c4c330e51e03b630fc33c
Amos Van Horn <amos.vanhorn@gmail.com>
2026-01-01 17:40:27 -0500

    addded account concept

--- BudgetCal/Models/AccountBalance.cs
+++
@@ -1,7 +0,0 @@
-namespace BudgetCal.Models;
-
-public class AccountBalance
-{
-    public static readonly DateTime StartDate = new DateTime(2025, 9, 1);
-    public static readonly decimal StartingBalance = 1000m;
-}
